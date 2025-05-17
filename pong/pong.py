f19a9c136df1cffc1f21060821901c2624fff77a
xXsnackXx <devonalderson@gmail.com>
2025-05-17 14:54:13 -0700

    pong window works

---
+++ pong/pong.py
@@ -0,0 +1,21 @@
+# Simple Pong in Python 3 for Beginners
+# By @voidland
+
+
+import turtle
+
+wn = turtle.Screen()
+wn.title("pong by @voidland")
+wn.bgcolor("black")
+wn.setup(width=800, height=600)
+wn.tracer(0)
+
+
+
+
+
+
+
+# Main game loop
+while True:
+    wn.update()
