4bc6f847c36c68b92a0e4ce8721cbbe39004152b
xXsnackXx <devonalderson@gmail.com>
2025-05-19 21:36:07 -0700

    yerp

--- CookieClicker/testing.py
+++ CookieClicker/testing.py
@@ -6,6 +6,8 @@ turtle.bgpic(r"C:\Users\Devon\Documents\GITREPOS\Python\CookieClicker\pxfuel.png
 wn = turtle.Screen()
 wn.title("Test window")
 wn.bgcolor("red")
+wn.setup(width=800, height=800)
+
 
 
 
