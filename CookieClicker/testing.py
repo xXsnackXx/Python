5c3ce13c6019ebfc0e7b083f72d2288e2e607850
xXsnackXx <devonalderson@gmail.com>
2025-05-19 20:50:47 -0700

    just some stuff

---
+++ CookieClicker/testing.py
@@ -0,0 +1,19 @@
+import turtle
+
+turtle.bgpic(r"C:\Users\Devon\Documents\GITREPOS\Python\CookieClicker\pxfuel.png")
+
+
+wn = turtle.Screen()
+wn.title("Test window")
+wn.bgcolor("red")
+
+
+
+
+
+
+
+
+
+
+wn.mainloop()
