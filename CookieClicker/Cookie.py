c07e20aa8d38c96f5c38569924948fab28d07594
xXsnackXx <devonalderson@gmail.com>
2025-05-19 19:51:10 -0700

    Yuki bag more photos

--- CookieClicker/Cookie.py
+++ CookieClicker/Cookie.py
@@ -4,6 +4,8 @@
 import turtle
 import winsound
 
+
+
 turtle.bgpic(r"C:\Users\Devon\Documents\GITREPOS\Python\CookieClicker\pxfuel.png")
 
 wn = turtle.Screen()
@@ -15,7 +17,11 @@ wn.setup(width=600, height=600)
 
 wn.register_shape(r"C:\Users\Devon\Documents\GITREPOS\Python\CookieClicker\yukicookie.gif")
 wn.register_shape(r"C:\Users\Devon\Documents\GITREPOS\Python\CookieClicker\yukicute.gif")
+wn.register_shape(r"C:\Users\Devon\Documents\GITREPOS\Python\CookieClicker\yukichair.gif")
+wn.register_shape(r"C:\Users\Devon\Documents\GITREPOS\Python\CookieClicker\yukibag.gif")
 
+
+# Setting first yuki
 cookie = turtle.Turtle()
 cookie.shape(r"C:\Users\Devon\Documents\GITREPOS\Python\CookieClicker\yukicookie.gif")
 cookie.speed(0)
@@ -37,6 +43,7 @@ message.color("cyan")
 message.penup()
 message.goto(0, -200)
 
+
 def clicked(x, y):
     global clicks
     clicks += 1
@@ -48,12 +55,30 @@ def clicked(x, y):
     if clicks == 50:
         cookie.shape(r"C:\Users\Devon\Documents\GITREPOS\Python\CookieClicker\yukicute.gif")
 
+    # Change Img at 100 clicks
+    if clicks == 100:
+        cookie.shape(r"C:\Users\Devon\Documents\GITREPOS\Python\CookieClicker\yukichair.gif")
+
+    # Change Img at 150 clicks
+    if clicks == 150:
+        cookie.shape(r"C:\Users\Devon\Documents\GITREPOS\Python\CookieClicker\yukibag.gif")
+
     # Milestone 50 clicks message
     if clicks == 50:
         message.write("You got a new Yuki Pic!", align="center", font=("Beach", 24, "bold"))
+        wn.ontimer(message.clear, 3000)
+
+         # Milestone 100 clicks message
+    if clicks == 100:
+        message.write("Chair Yuki!", align="center", font=("Beach", 24, "bold"))
+        wn.ontimer(message.clear, 3000)
+
+        # Milestone 150 clicks message 
+    if clicks == 150:
+        message.write("Yuki got a bag upgrade!", align="center", font=("Beach", 24, "bold"))
+        wn.ontimer(message.clear, 3000)
 
 cookie.onclick(clicked)
-
 
 
 wn.mainloop()
