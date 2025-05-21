c5144cd0b8c4f74f8aace67e0c179d3c686193c5
xXsnackXx <devonalderson@gmail.com>
2025-05-20 22:56:08 -0700

    yeppsake

--- Snake/Snake.py
+++ Snake/Snake.py
@@ -2,3 +2,49 @@
 # By @Voidland
 
 import turtle
+import time
+
+delay = 0.1
+
+turtle.bgpic(r"C:\Users\Devon\Documents\GITREPOS\Python\Snake\animesnakebg.png")
+
+
+# Set up Screen 
+
+wn = turtle.Screen()
+wn.title("Snake Game by @Voidland")
+wn.bgcolor("black")
+wn.setup(width=600, height=600)
+wn.tracer(0)
+
+# Snake Head
+head = turtle.Turtle()
+head.speed(0)
+head.shape("circle")
+head.color("#00FF00")
+head.penup()
+head.goto(0, 0)
+head.direction = "stop"
+
+# Functions
+def move():
+    if head.direction == "up":
+        y = head.ycor()
+        head.sety(y + 20)
+
+# Main Game loop
+# while True:
+    wn.update()
+
+    move()
+
+    time.sleep(delay)
+
+    
+
+
+
+
+
+
+wn.mainloop()
