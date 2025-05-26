11b5b734579e217da08fe11d6138c50824b139bd
xXsnackXx <devonalderson@gmail.com>
2025-05-26 01:33:34 -0700

    weird text fomatting

---
+++ Misc/Printtest.py
@@ -0,0 +1,8 @@
+# surrounding text with symbols ^ is around text < is right of text > is left of text
+
+text = 'epic'
+
+print(f'{text}')
+print(f'{text:#^20}')
+print(f'{text:#<20}')
+print(f'{text:#>20}')
