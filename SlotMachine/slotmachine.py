d954f2f1741fe1d816d68b30c591d275fd382236
xXsnackXx <devonalderson@gmail.com>
2025-05-25 18:55:52 -0700

    finished slot macine somtimes i win on line 4 when there is not supposed to be a line 4 problem for later

--- SlotMachine/slotmachine.py
+++ SlotMachine/slotmachine.py
@@ -8,8 +8,8 @@ import random
 MAX_LINES = 3
 MAX_BET = 100     # Can change to update program
 MIN_BET = 1
-ROW = 3
-COL = 3
+ROWS = 3
+COLS = 3
 # Constant varibles---------------
 
 symbol_count = {
@@ -19,28 +19,56 @@ symbol_count = {
     "D": 8
 }
 
+symbol_value = {
+    "A": 5,
+    "B": 4,
+    "C": 3,
+    "D": 2
+}
+
+def check_winnings(columns, lines, bet, values):
+    winnings = 0
+    winning_lines = []
+    for line in range(lines):
+        symbol = columns[0][line]
+        for column in columns:
+            symbol_to_check = column[line]
+            if symbol != symbol_to_check:
+                break
+        else:
+            winnings += values[symbol] * bet
+            winning_lines.append(lines + 1)
+
+        return winnings, winning_lines
+
 def get_slot_machine_spin(rows, cols, symbols):
     all_symbols = []
     for symbol, symbol_count in symbols.items():
         for _ in range(symbol_count):
             all_symbols.append(symbol)
 
-    columns = [[], [], []]
-    for col in range(rows):
+    columns = []
+    for _ in range(cols):
         column = []
         current_symbols = all_symbols[:]
-        for row in range(rows):
-            value = random.choice(all_symbols)
+        for _ in range(rows):
+            value = random.choice(current_symbols)
             current_symbols.remove(value)
             column.append(value)
+
         columns.append(column)
     
     return columns
 
 def print_slot_machine(columns):
     for row in range(len(columns[0])):
-        for column in columns:
-            print(column[row], "|")
+        for i, column in enumerate(columns):
+            if i != len(columns) - 1:
+                print(column[row], end= " | ")
+            else:
+                print(column[row], end= "")
+
+        print()
         
 
 # User geting deposit function
@@ -70,6 +98,7 @@ def get_number_of_lines():
                 print("Amount must be 1, 2, or 3.")
         else:
             print("Please enter a number.")
+    
     return lines
 
 # User geting bet function
@@ -84,11 +113,12 @@ def get_bet():
                 print(f"Amount must be between {MIN_BET} - {MAX_BET}")#f with curly brakets inputs var into string
         else:
             print("Please enter a number.")
+    
     return amount
 
 # Main program loop calls 
-def main():
-    balance = deposit()
+
+def spin(balance):
     lines = get_number_of_lines()
     while True:
         bet = get_bet()
@@ -101,6 +131,24 @@ def main():
         
     
     print(f"You are betting ${bet} on {lines} lines. Total bet is equal to: ${total_bet} ")
+
+    slots = get_slot_machine_spin(ROWS, COLS, symbol_count)
+    print_slot_machine(slots)
+    winnings, winning_lines = check_winnings(slots, lines, bet, symbol_value)
+    print(f"You won ${winnings}.")
+    print(f"You won on line:", *winning_lines)
+    return winnings - total_bet
+
+def main():
+    balance = deposit()
+    while True:
+        print(f"Current balance is ${balance}")
+        answer = input("Press enter to play (q to quit).")
+        if answer == "q":
+            break
+        balance += spin(balance)
+    
+    print(f"You left with ${balance}")
 
 
 
