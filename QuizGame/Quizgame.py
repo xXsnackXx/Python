bb648709e7876ed9450f9271510c97d7262a8027
xXsnackXx <devonalderson@gmail.com>
2025-05-27 23:10:06 -0700

    updated working with score

--- QuizGame/Quizgame.py
+++ QuizGame/Quizgame.py
@@ -1,19 +1,49 @@
 # quiz game python tutorial 
 
+score = 0
+
 print("Welcome to my leauge quiz")
 
 playing = input("Want to take my leauge quiz? ")
 
-if playing != "yes":
+if playing != "yes".lower():
     print("fuck you")
     quit()
 print("Okay lets play")
 
-answer = input("where did kaisa get her powers from? ")
-if answer == "void, the void":
+answer = input("where did kaisa get her powers from? ").lower()
+if answer in ["void", "the void", "from the void"]:
     print("Correct!")
+    score += 1
+else:
+    print("Nope!")
+    print("Nope!")  
+   
+answer = input("What race is Tristana? ").lower()
+if answer in ["yordle"]:
+    print("Correct!")
+    score += 1
+else:
+    print("Nope!")
+    print("Nope!") 
 
 
+answer = input("What area is Darius from? ").lower()
+if answer in ["noxus"]:
+    print("Correct!")
+    score += 1
+else:
+    print("Nope!")
+    print("Nope!")
+   
+answer = input("what was the old 3v3 map called? ").lower()
+if answer in ["twisted treeline"]:
+    print("Correct!")
+    score += 1
+else:
+    print("Nope!")
+    print("Nope!")
 
+print(f"You got {score} of 4 questions correct!")
                
     
