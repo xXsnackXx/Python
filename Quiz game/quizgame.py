7684635e068996decd3014c8453da5d9e52e6802
xXsnackXx <devonalderson@gmail.com>
2025-05-27 20:58:52 -0700

    yep

--- Quiz game/quizgame.py
+++ Quiz game/quizgame.py
@@ -2,4 +2,6 @@ print("Welcome to my leauge game")
 
 playing = input("Do you want to play? ")
 
-
+if playing != "yes":
+    print("fuck you")
+    
--- QuizGame/quizgame.py
+++
@@ -1,6 +0,0 @@
-print("Welcome to my leauge game")
-
-playing = input("Do you want to play? ")
-
-if input == yes
-print("I bet you do")
