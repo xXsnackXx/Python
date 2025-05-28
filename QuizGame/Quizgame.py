a20192aa293d7d815a21a510403a2c0a48490fda
xXsnackXx <devonalderson@gmail.com>
2025-05-27 21:37:36 -0700

    updatequiz

---
+++ QuizGame/Quizgame.py
@@ -0,0 +1,19 @@
+# quiz game python tutorial 
+
+print("Welcome to my leauge quiz")
+
+playing = input("Want to take my leauge quiz? ")
+
+if playing != "yes":
+    print("fuck you")
+    quit()
+print("Okay lets play")
+
+answer = input("where did kaisa get her powers from? ")
+if answer == "void, the void":
+    print("Correct!")
+
+
+
+               
+    
