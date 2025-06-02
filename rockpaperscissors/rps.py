fe1c62951ff7c945ac9e5630e5f0b7fdb5968b3d
xXsnackXx <devonalderson@gmail.com>
2025-06-01 18:19:20 -0700

    gg

--- rockpaperscissors/rps.py
+++ rockpaperscissors/rps.py
@@ -7,19 +7,34 @@ computer_wins = 0
 options = ["rock", "paper", "scissors"]
 
 while True:
-    user_input = input("Type Rock/Paper/Scissors or Q to quit: ")
+    user_input = input("Type Rock/Paper/Scissors or Q to quit: ").lower()
     if user_input == "q":
         break
     if user_input not in options: 
         continue
 
-    random_number = random.randint()
-    #r rock: 0, paper: 2, scissors: 2
+    random_number = random.randint(0, 2)
+    # rock: 0, paper: 2, scissors: 2
     computer_pick = options[random_number]
     print("Computer picked", computer_pick + ".")
 
     if user_input == "rock" and computer_pick == "scissors":
         print("You won!")
-    user_wins += 1
+        user_wins += 1
+        
+
+    elif user_input == "paper" and computer_pick == "rock":
+        print("You won!")
+        user_wins += 1
+        
+
+    elif user_input == "scissors" and computer_pick == "paper":
+        print("You won!")
+        user_wins += 1
+    else:
+        print("You lost!")    
+        computer_wins =+ 1
+print("You won")
+print("The Computer won!", computer_wins, "times.")
 print("Goodbye")
 
